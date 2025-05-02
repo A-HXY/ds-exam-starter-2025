27286e023d0d2d2f2c20f5c66dda141eba4c8522
Xinyue <858937756@qq.com>
2025-05-02 10:04:06 +0100

    modify existsFilter

--- lib/exam-stack.ts
+++ lib/exam-stack.ts
@@ -115,7 +115,7 @@ export class ExamStack extends cdk.Stack {
     topic1.addSubscription(
       new subs.SqsSubscription(queueB, {
         filterPolicy: {
-          email: sns.SubscriptionFilter.existsFilter(false),
+          email: sns.SubscriptionFilter.exists(false),
         },
       })
     );
